5724fd69e015ce24b37d0c0e07e89ef0eb314ff1
DialecticalLaw <d.shmuratkin@mail.ru>
2024-07-03 20:22:23 +0300

    feat: add styles for the Search

--- class-components/src/components/Search/Search.tsx
+++ class-components/src/components/Search/Search.tsx
@@ -3,6 +3,13 @@ import styles from './Search.module.css';
 
 export class Search extends Component {
   render(): ReactNode {
-    return <section className={styles.search_section}>search</section>;
+    return (
+      <section className={styles.search_section}>
+        <input className={styles.input} type="search" placeholder="Type something..." />
+        <button className={styles.btn} type="button">
+          Search
+        </button>
+      </section>
+    );
   }
 }
