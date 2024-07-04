28e5c03756f678575c0b5bc6ca6794ca465ec8fe
DialecticalLaw <d.shmuratkin@mail.ru>
2024-07-04 12:49:12 +0300

    feat: add styles for the Results

--- class-components/src/components/Results/Results.tsx
+++ class-components/src/components/Results/Results.tsx
@@ -4,6 +4,33 @@ import { Starship } from '../../services/API_service';
 
 export class Results extends Component<{ items: Starship[] }> {
   render(): ReactNode {
-    return <section className={styles.results_section}>{JSON.stringify(this.props.items)}</section>;
+    return (
+      <section className={styles.results_section}>
+        {this.props.items.map((item) => {
+          return (
+            <div className={styles.item}>
+              <h1 className={styles.item_title}>{item.name}</h1>
+
+              <p className={styles.item_prop}>
+                Model: <span className={styles.item_value}>{item.model}</span>
+              </p>
+
+              <p className={styles.item_prop}>
+                Manufacturer: <span className={styles.item_value}>{item.manufacturer}</span>
+              </p>
+
+              <p className={styles.item_prop}>
+                Max atmospheric speed:{' '}
+                <span className={styles.item_value}>{item.max_atmosphering_speed}</span>
+              </p>
+
+              <p className={styles.item_prop}>
+                Crew: <span className={styles.item_value}>{item.crew}</span>
+              </p>
+            </div>
+          );
+        })}
+      </section>
+    );
   }
 }
