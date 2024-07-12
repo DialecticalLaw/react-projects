524a147ae0e464d96c7993ee751f347cf452f2f1
DialecticalLaw <d.shmuratkin@mail.ru>
2024-07-12 21:20:12 +0300

    refactor: convert Loader into a functional component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,13 +1,10 @@
-import { Component } from 'react';
 import styles from './Loader.module.css';
 import loaderSvg from '../../assets/img/infinite-spinner.svg';
 
-export class Loader extends Component {
-  render() {
-    return (
-      <div className={styles.loader_wrapper}>
-        <img className={styles.loader} src={loaderSvg} alt="loader" />
-      </div>
-    );
-  }
+export function Loader() {
+  return (
+    <div className={styles.loader_wrapper}>
+      <img className={styles.loader} src={loaderSvg} alt="loader" />
+    </div>
+  );
 }
