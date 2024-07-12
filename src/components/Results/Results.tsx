e96fe8388d8f8c894a360f3ab5e42531d3524fa1
DialecticalLaw <d.shmuratkin@mail.ru>
2024-07-12 21:19:18 +0300

    refactor: convert Results into a functional component

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,26 +1,23 @@
-import { Component, ReactNode } from 'react';
 import styles from './Results.module.css';
 import { Film } from '../../services/API_service';
 
-export class Results extends Component<{ items: Film[] }> {
-  render(): ReactNode {
-    return (
-      <section className={styles.results_section}>
-        {this.props.items.map((item) => {
-          return (
-            <div key={item.release_date} className={styles.item}>
-              <h1 className={styles.item_title}>{item.title}</h1>
+export function Results({ items }: { items: Film[] }) {
+  return (
+    <section className={styles.results_section}>
+      {items.map((item) => {
+        return (
+          <div key={item.release_date} className={styles.item}>
+            <h1 className={styles.item_title}>{item.title}</h1>
 
-              <p className={styles.item_prop}>
-                Director: <span className={styles.item_value}>{item.director}</span>
-              </p>
-              <p className={styles.item_prop}>
-                Release: <span className={styles.item_value}>{item.release_date}</span>
-              </p>
-            </div>
-          );
-        })}
-      </section>
-    );
-  }
+            <p className={styles.item_prop}>
+              Director: <span className={styles.item_value}>{item.director}</span>
+            </p>
+            <p className={styles.item_prop}>
+              Release: <span className={styles.item_value}>{item.release_date}</span>
+            </p>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
