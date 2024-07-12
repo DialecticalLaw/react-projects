3b111bf9b3ea82368c4242ae9a5379f94251ff7d
DialecticalLaw <d.shmuratkin@mail.ru>
2024-07-12 21:38:40 +0300

    feat: change the search for films to search for planets

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,19 +1,19 @@
 import styles from './Results.module.css';
-import { Film } from '../../services/API_service';
+import { Planet } from '../../services/API_service';
 
-export function Results({ items }: { items: Film[] }) {
+export function Results({ items }: { items: Planet[] }) {
   return (
     <section className={styles.results_section}>
       {items.map((item) => {
         return (
-          <div key={item.release_date} className={styles.item}>
-            <h1 className={styles.item_title}>{item.title}</h1>
+          <div key={item.name} className={styles.item}>
+            <h1 className={styles.item_title}>{item.name}</h1>
 
             <p className={styles.item_prop}>
-              Director: <span className={styles.item_value}>{item.director}</span>
+              Population: <span className={styles.item_value}>{item.population}</span>
             </p>
             <p className={styles.item_prop}>
-              Release: <span className={styles.item_value}>{item.release_date}</span>
+              Terrain: <span className={styles.item_value}>{item.terrain}</span>
             </p>
           </div>
         );
--- src/services/API_service.ts
+++ src/services/API_service.ts
@@ -1,22 +1,26 @@
-export interface Film {
-  title: string;
-  director: string;
-  release_date: string;
-  opening_crawl: string;
-  producer: string;
+export interface Planet {
+  name: string;
+  population: string;
+  terrain: string;
+  orbital_period: string;
+  rotation_period: string;
+  surface_water: string;
+  gravity: string;
+  diameter: string;
+  climate: string;
 }
 
 interface APIResponse {
   count: number;
   next: string | null;
   previous: string | null;
-  results: Film[];
+  results: Planet[];
 }
 
 export class API_service {
-  private baseUrl = 'https://swapi.dev/api/films/';
+  private baseUrl = 'https://swapi.dev/api/planets/';
 
-  public async searchItems(searchTerm: string): Promise<Film[] | undefined> {
+  public async searchItems(searchTerm: string): Promise<Planet[] | undefined> {
     try {
       const response = await fetch(`${this.baseUrl}?search=${searchTerm}`);
       const result: APIResponse = await response.json();
