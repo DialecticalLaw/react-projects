42485ffec348d5ff64217151c15ae836c99b4374
DialecticalLaw <d.shmuratkin@mail.ru>
2024-07-13 11:37:52 +0300

    chore: setup react-router

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import './global.css';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([{ path: '/', element: <App /> }]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>
 );
