e71d6b0494d18d31545d8c2406f0f7f46a20e07e
DialecticalLaw <d.shmuratkin@mail.ru>
2024-07-19 18:11:54 +0300

    feat: create redux store

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import './global.css';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routerConfig } from './routes/routerConfig.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 const router = createBrowserRouter(routerConfig);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
+    <Provider store={store}>
+      <ErrorBoundary>
+        <RouterProvider router={router} />
+      </ErrorBoundary>
+    </Provider>
   </React.StrictMode>
 );
