0421b37220cc31ba7a81a5bff15abc418d56f172
DialecticalLaw <d.shmuratkin@mail.ru>
2024-07-21 19:41:59 +0300

    refactor: move items in the separate component

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,41 +1,15 @@
 import styles from './Results.module.css';
-import { useSearchParams } from 'react-router-dom';
-import { extractId } from '../../helpers/extractId';
 import { Planet } from '../../services/planets';
-import { useContext } from 'react';
-import { ThemeContext } from '../../store/ThemeContext';
+import { Item } from './Item/Item';
 
 export function Results({ items }: { items?: Planet[] }) {
-  const [, setSearchParams] = useSearchParams();
-  const { theme } = useContext(ThemeContext);
-
   if (!items || !items.length) return <p className={styles.text}>Nothing was found</p>;
 
   return (
     <section className={styles.results_section}>
-      {items.map((item) => {
-        return (
-          <div
-            key={item.name}
-            className={`${styles.item} ${theme === 'light' ? styles.light : ''}`}
-            onClick={() =>
-              setSearchParams((prev) => {
-                prev.set('details', extractId(item.url));
-                return prev;
-              })
-            }
-          >
-            <h1 className={styles.item_title}>{item.name}</h1>
-
-            <p className={styles.item_prop}>
-              Population: <span className={styles.item_value}>{item.population}</span>
-            </p>
-            <p className={styles.item_prop}>
-              Terrain: <span className={styles.item_value}>{item.terrain}</span>
-            </p>
-          </div>
-        );
-      })}
+      {items.map((item) => (
+        <Item key={item.url} item={item} />
+      ))}
     </section>
   );
 }
