e4fb2dfc38f3fc3fab7f615480a7534b07e31450
DialecticalLaw <d.shmuratkin@mail.ru>
2024-08-07 18:02:50 +0300

    refactor: remove redux from project

--- src/services/planets.ts
+++ src/services/planets.ts
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export interface Planet {
   name: string;
@@ -20,18 +20,18 @@ export interface ApiResponse {
   results: Planet[];
 }
 
-export const planetsApi = createApi({
-  reducerPath: 'planetsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://swapi.dev/api/planets/' }),
-  endpoints: (builder) => ({
-    getItems: builder.query<ApiResponse, { searchTerm: string; page: number }>({
-      query: ({ searchTerm, page }) => `?search=${searchTerm}&page=${page}`
-    }),
-    getItemById: builder.query<Planet, string | null>({
-      query: (id) => {
-        if (!id) throw new Error('invalid id');
-        return id;
-      }
-    })
-  })
-});
+// export const planetsApi = createApi({
+//   reducerPath: 'planetsApi',
+//   baseQuery: fetchBaseQuery({ baseUrl: 'https://swapi.dev/api/planets/' }),
+//   endpoints: (builder) => ({
+//     getItems: builder.query<ApiResponse, { searchTerm: string; page: number }>({
+//       query: ({ searchTerm, page }) => `?search=${searchTerm}&page=${page}`
+//     }),
+//     getItemById: builder.query<Planet, string | null>({
+//       query: (id) => {
+//         if (!id) throw new Error('invalid id');
+//         return id;
+//       }
+//     })
+//   })
+// });
