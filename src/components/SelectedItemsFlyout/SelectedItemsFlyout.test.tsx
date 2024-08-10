3ab9d76033fd879a390103a566aa2bebea1f2e1c
DialecticalLaw <d.shmuratkin@mail.ru>
2024-08-10 21:39:16 +0300

    chore: add tests for SelectedItemsFlyout

---
+++ src/components/SelectedItemsFlyout/SelectedItemsFlyout.test.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import { SelectedItemsFlyout } from './SelectedItemsFlyout';
+import { SelectedItemsContext } from '../../store/SelectedItemsContext';
+import { Planet } from '../../interfaces';
+
+describe('SelectedItemsFlyout', () => {
+  it('displays correctly', async () => {
+    const planetsArray = [{}, {}, {}] as Planet[];
+    const clearSelectedItems = () => {
+      planetsArray.length = 0;
+    };
+    global.URL.createObjectURL = vi.fn();
+    global.URL.revokeObjectURL = vi.fn();
+
+    const { getByText } = render(
+      <SelectedItemsContext.Provider
+        value={{ selectedItems: planetsArray, setSelectedItems: clearSelectedItems }}
+      >
+        <SelectedItemsFlyout />
+      </SelectedItemsContext.Provider>
+    );
+
+    expect(getByText('items are selected').parentElement).toHaveTextContent('3 items are selected');
+  });
+});
