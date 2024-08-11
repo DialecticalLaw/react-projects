d55e4b254b6412b0f27f7c59498a156b88799732
DialecticalLaw <d.shmuratkin@mail.ru>
2024-08-12 00:08:58 +0300

    chore: correct tests for Results

--- components/Results/Results.test.tsx
+++ components/Results/Results.test.tsx
@@ -1,77 +1,36 @@
-// import { render } from '@testing-library/react';
-// import { describe, expect, it } from 'vitest';
-// import { Results } from './Results';
-// import { MemoryRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { store } from '../../store/store';
-// import { Planet } from '../../interfaces';
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { Results } from './Results';
+import { ApiResponse } from '../../interfaces';
+import { createRemixStub } from '@remix-run/testing';
 
-// describe('Results', () => {
-//   it('renders the specified number of cards', () => {
-//     const items = [
-//       {
-//         name: 'Dagobah',
-//         population: 'unknown',
-//         terrain: 'swamp, jungles',
-//         url: 'https://swapi.dev/api/planets/1/'
-//       },
-//       {
-//         name: 'Tatooine',
-//         population: '200000',
-//         terrain: 'desert',
-//         url: 'https://swapi.dev/api/planets/2/'
-//       },
-//       {
-//         name: 'Endor',
-//         population: '30000000',
-//         terrain: 'forests, mountains, lakes',
-//         url: 'https://swapi.dev/api/planets/3/'
-//       }
-//     ] as Planet[];
+describe('Results', () => {
+  it('renders the specified number of cards', async () => {
+    const apiRes: ApiResponse = await (await fetch('https://swapi.dev/api/planets/')).json();
+    const RemixStub = createRemixStub([
+      {
+        path: '/',
+        Component: function () {
+          return <Results items={apiRes.results} />;
+        }
+      }
+    ]);
+    const { getAllByText } = render(<RemixStub />);
 
-//     const { getAllByText } = render(
-//       <Provider store={store}>
-//         <MemoryRouter>
-//           <Results items={items} />
-//         </MemoryRouter>
-//       </Provider>
-//     );
+    expect(getAllByText('Population:').length).toBe(2);
+  });
 
-//     expect(getAllByText('Population:').length).toBe(3);
-//   });
+  it('appropriate message is displayed if no cards are present', () => {
+    const RemixStub = createRemixStub([
+      {
+        path: '/',
+        Component: function () {
+          return <Results items={[]} />;
+        }
+      }
+    ]);
+    const { getByText } = render(<RemixStub />);
 
-//   it('appropriate message is displayed if no cards are present', () => {
-//     const { getByText } = render(
-//       <Provider store={store}>
-//         <MemoryRouter>
-//           <Results items={[]} />
-//         </MemoryRouter>
-//       </Provider>
-//     );
-
-//     expect(getByText('Nothing was found')).toBeInTheDocument();
-//   });
-
-//   it('renders the relevant card data', () => {
-//     const items = [
-//       {
-//         name: 'Dagobah',
-//         population: 'unknown',
-//         terrain: 'swamp, jungles',
-//         url: 'https://swapi.dev/api/planets/1/'
-//       }
-//     ] as Planet[];
-
-//     const { getByText } = render(
-//       <Provider store={store}>
-//         <MemoryRouter>
-//           <Results items={items} />
-//         </MemoryRouter>
-//       </Provider>
-//     );
-
-//     expect(getByText('Dagobah')).toBeInTheDocument();
-//     expect(getByText('unknown')).toBeInTheDocument();
-//     expect(getByText('swamp, jungles')).toBeInTheDocument();
-//   });
-// });
+    expect(getByText('Nothing was found')).toBeInTheDocument();
+  });
+});
