5ef3f4b9c55466bd130d87847ee4529ab5cd8589
DialecticalLaw <d.shmuratkin@mail.ru>
2024-08-11 15:49:05 +0300

    feat: add metadata

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,10 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '!**/.server', '!**/.client'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'react-compiler'],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react-compiler/react-compiler': 'error',
     '@typescript-eslint/no-explicit-any': 'error'
   }
--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,22 @@
 import { Links, Meta, Outlet, Scripts } from '@remix-run/react';
+import globalCss from './global.css?url';
+import { LinksFunction } from '@remix-run/node';
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: globalCss }];
 
 export default function App() {
   return (
-    <html>
+    <html lang="en">
       <head>
-        <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <link rel="icon" type="image/svg+xml" href="./public/planet.ico" />
+        <title>Star Wars - Planets search</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
         <h1>Hello world!</h1>
         <Outlet />
-
         <Scripts />
       </body>
     </html>
