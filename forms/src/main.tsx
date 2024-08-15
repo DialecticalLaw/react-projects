b1519b657e427549e96e16b3d6d63ef309c56f2b
DialecticalLaw <d.shmuratkin@mail.ru>
2024-08-15 22:31:45 +0300

    feat: add routing

--- forms/src/main.tsx
+++ forms/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home } from './components/Home/Home.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: 'controlled',
+    element: <div />
+  },
+  {
+    path: 'uncontrolled',
+    element: <div />
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
